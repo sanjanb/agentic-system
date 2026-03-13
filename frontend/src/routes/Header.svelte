43c8cee9dfe578c0eb53909a275be42f84758bca
Sanjan B M <acharyasanjan931@gmail.com>
2026-03-13 11:11:34 +0530

    fix: remove 'About' link from navigation in Header component

--- frontend/src/routes/Header.svelte
+++ frontend/src/routes/Header.svelte
@@ -26,9 +26,6 @@
 			<li aria-current={page.url.pathname === '/query' ? 'page' : undefined}>
 				<a href={resolve('/query')}>RAG Query</a>
 			</li>
-			<li aria-current={page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href={resolve('/about')}>About</a>
-			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
